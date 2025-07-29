14d022fcc400b6cec6fca292971d55e242e67ff9
Ahmed Esam <buhussien2@gmail.com>
2025-07-29 16:35:01 +0300

    Add files via upload

---
+++ Task Factory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelExample
+{
+    internal class Program
+    {
+        static List<string> urls = new List<string>
+        {
+            "https://www.cnn.com",
+            "https://www.amazon.com",
+            "https://www.programmingadvices.com",
+            "https://www.Facebook.com",
+            "https://www.google.com",
+          
+
+        };
+        static void DownloadContent(string url)
+        {
+            string content;
+
+
+            using (WebClient client = new WebClient())
+            {
+                // Simulate some work by adding a delay
+                Thread.Sleep(100);
+
+
+                // Download the content of the web page
+                content = client.DownloadString(url);
+            }
+
+
+            Console.WriteLine($"{url}: {content.Length} characters downloaded");
+        }
+        static void Main(string[] args)
+        {
+            
+
+            Parallel.ForEach(urls,url =>
+            {
+               DownloadContent(url);
+            });
+
+            Console.WriteLine("All Completed");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
